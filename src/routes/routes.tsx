b1444c3a8e79753ea3f36257db0faaff7b4a4abf
VictorRevers <victorrkasnowski@gmail.com>
2024-05-16 16:55:47 -0300

    expo router added

---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home } from "../screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+export const Routes = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{ headerShown: false, gestureEnabled: false }}
+        >
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+    );
+};
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@gluestack-ui/themed';
+
+export const Home = () => {
+    return (
+        <Text>Home O.</Text>
+    );
+}
+
+
+
