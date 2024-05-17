8cb59969e149f32f0c1f961b36f9761c266ec748
VictorRevers <victorrkasnowski@gmail.com>
2024-05-17 14:54:33 -0300

    starting image controller fix

--- src/controllers/ImagesController.tsx
+++ src/controllers/ImagesController.tsx
@@ -5,10 +5,10 @@ let imagesList: Array<string> = [];
 export default class ImagesController{
   
     static GetImages(type:string){
-        
+        imagesList = fs.readdirSync(`../assets/Images/${type}`);
     }
 
     static SelectImage(){
-
+        imagesList
     }
 }
