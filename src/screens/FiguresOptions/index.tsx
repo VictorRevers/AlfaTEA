155778067ff182e345284db787c0497200d69b3a
VictorRevers <victorrkasnowski@gmail.com>
2024-05-28 16:49:57 -0300

    print screen

--- src/screens/FiguresOptions/index.tsx
+++ src/screens/FiguresOptions/index.tsx
@@ -1,13 +1,40 @@
-import { View, Text, Image } from "@gluestack-ui/themed";
+import { View, Text, Image, Button } from "@gluestack-ui/themed";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { NavigationProp } from "@react-navigation/native";
+import { captureRef } from 'react-native-view-shot';
+import { useEffect, useState, useRef } from 'react';
+import {PixelRatio} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
 
 export const FiguresOptions = ({
   navigation,
 }: {
   navigation: NavigationProp<any>;
 }) => {
+  const viewToSnapShotRef:any = useRef();
+  const [snapshotImage,setSnapShotImage] = useState('');
+
+  const targetPixelCount = 1080; // If you want full HD pictures
+  const pixelRatio = PixelRatio.get(); // The pixel ratio of the device
+  // pixels * pixelRatio = targetPixelCount, so pixels = targetPixelCount / pixelRatio
+  const pixels = targetPixelCount / pixelRatio;
+
+  const snapshot = async() =>{
+    const result = await captureRef(viewToSnapShotRef, {
+    result: 'tmpfile',
+    height: pixels,
+    width: pixels,
+    quality: 1,
+    format: 'png',
+  });
+  console.log(result);
+  setSnapShotImage(result);
+}
+
   return (
     <View
       display="flex"
@@ -88,7 +115,8 @@ export const FiguresOptions = ({
           </Text>
         </View>
       </View>
-      <View
+      <SafeAreaView></SafeAreaView>
+      <View ref={viewToSnapShotRef}
         display="flex"
         alignItems="center"
         justifyContent="center"
@@ -124,7 +152,7 @@ export const FiguresOptions = ({
         <View>
           <Text>Stars </Text>
         </View>
-        <MaterialCommunityIcons name="printer" size={32} color="black" />
+        <Button onPress={snapshot}><MaterialCommunityIcons name="printer" size={32} color="black" /></Button>
       </View>
     </View>
   );
