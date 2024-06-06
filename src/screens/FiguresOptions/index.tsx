bcf85bd4a6cb355cb84f8ddea00138b1a1b6173f
Mateus <mateuslopes1717@gmail.com>
2024-06-06 12:18:21 -0300

    Keyboard working

--- src/screens/FiguresOptions/index.tsx
+++ src/screens/FiguresOptions/index.tsx
@@ -1,26 +1,30 @@
-import { View, Text, Image, Link, Pressable, Button } from "@gluestack-ui/themed";
+import {
+  View,
+  Text,
+  Image,
+  Link,
+  Pressable,
+  Button,
+} from "@gluestack-ui/themed";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { NavigationProp } from "@react-navigation/native";
-import { captureRef } from 'react-native-view-shot';
-import {PixelRatio} from "react-native";
+import { captureRef } from "react-native-view-shot";
+import { PixelRatio } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PrintPDF from "../../controllers/PrintPDF";
 import ImagesController from "../../controllers/ImagesController";
 
-
-
 import { Context } from "../../../App";
 import { useContext, useState, useEffect, useRef } from "react";
-
 
 export const FiguresOptions = ({
   navigation,
 }: {
   navigation: NavigationProp<any>;
 }) => {
-  const viewToSnapShotRef:any = useRef();
-  const [snapshotImage,setSnapShotImage] = useState('');
+  const viewToSnapShotRef: any = useRef();
+  const [snapshotImage, setSnapShotImage] = useState("");
   const [images, setImages] = useState(Array<any>);
 
   const targetPixelCount = 1080; // If you want full HD pictures
@@ -29,34 +33,34 @@ export const FiguresOptions = ({
   const pixels = targetPixelCount / pixelRatio;
 
   //take printscreen and turn into PDF
-  const snapshot = async() =>{
+  const snapshot = async () => {
     const result = await captureRef(viewToSnapShotRef, {
-    result: 'data-uri',
-    height: pixels,
-    width: pixels,
-    quality: 1,
-    format: 'png',
-  });
-  console.log(result);
-  const html = await PrintPDF.setHTML(result);
-  PrintPDF.printToFile(html);
-}
+      result: "data-uri",
+      height: pixels,
+      width: pixels,
+      quality: 1,
+      format: "png",
+    });
+    console.log(result);
+    const html = await PrintPDF.setHTML(result);
+    PrintPDF.printToFile(html);
+  };
 
-  const getImages = (type:string) =>{
+  const getImages = (type: string) => {
     let imgs = ImagesController.GetImages(type);
     setImages(imgs);
     console.log("CLICOU!!!!!!");
 
     //implement change view...
-  }
+  };
 
   const [selectedImage, setSelectedImage] = useContext(Context);
   const [points, setPoints] = useState(0);
 
-  const addPoints = ()=>{
+  const addPoints = () => {
     let i: number = points + 1;
-    setPoints(i)
-  }
+    setPoints(i);
+  };
 
   return (
     <View
@@ -73,13 +77,15 @@ export const FiguresOptions = ({
         borderColor="$black"
         mt={6}
         borderWidth={"$1"}
-        w={"80%"}
+        w={"90%"}
+        h={"30%"}
         gap={6}
         p={3}
       >
         <View flexDirection="column" alignItems="center">
-          <Link
+          <Pressable
             onPress={() => {
+              getImages("Cenarios");
               setSelectedImage("Cenários");
               navigation.navigate("Game");
             }}
@@ -96,24 +102,13 @@ export const FiguresOptions = ({
             <Text fontSize={"$lg"} fontWeight={"$bold"}>
               Cenários
             </Text>
-          </Link>
-          <Pressable onPress={()=>{getImages("Cenarios")}}>
-            <Image
-              size="md"
-              alt="Image1"
-              source={{
-                uri: "http://placekitten.com/300/300",
-              }}           
-            />
-            <Text fontSize={"$lg"} fontWeight={"$bold"}>
-              Cenários
-            </Text>
           </Pressable>
         </View>
 
         <View flexDirection="column" alignItems="center">
-          <Link
+          <Pressable
             onPress={() => {
+              getImages("Personagens");
               setSelectedImage("Personagens");
               navigation.navigate("Game");
             }}
@@ -130,23 +125,12 @@ export const FiguresOptions = ({
             <Text fontSize={"$lg"} fontWeight={"$bold"}>
               Personagens
             </Text>
-          </Link>
-          <Pressable onPress={()=>{getImages("Personagens")}}>
-            <Image
-              size="md"
-              alt="Image1"
-              source={{
-                uri: "http://placekitten.com/300/300",
-              }}
-            />
-            <Text fontSize={"$lg"} fontWeight={"$bold"}>
-              Personagens
-            </Text>
           </Pressable>
         </View>
         <View flexDirection="column" alignItems="center">
-          <Link
+          <Pressable
             onPress={() => {
+              getImages("Animais");
               setSelectedImage("Animais");
               navigation.navigate("Game");
             }}
@@ -163,23 +147,12 @@ export const FiguresOptions = ({
             <Text fontSize={"$lg"} fontWeight={"$bold"}>
               Animais
             </Text>
-          </Link>
-          <Pressable onPress={()=>{getImages("Animais")}}>
-            <Image
-              size="md"
-              alt="Image1"
-              source={{
-                uri: "http://placekitten.com/300/300",
-              }}
-            />
-            <Text fontSize={"$lg"} fontWeight={"$bold"}>
-              Animais
-            </Text>
           </Pressable>
         </View>
         <View flexDirection="column" alignItems="center">
-          <Link
+          <Pressable
             onPress={() => {
+              getImages("Objetos");
               setSelectedImage("Objetos");
               navigation.navigate("Game");
             }}
@@ -196,23 +169,12 @@ export const FiguresOptions = ({
             <Text fontSize={"$lg"} fontWeight={"$bold"}>
               Objetos
             </Text>
-          </Link>
-          <Pressable onPress={()=>{getImages("Objetos")}}>
-            <Image
-              size="md"
-              alt="Image1"
-              source={{
-                uri: "http://placekitten.com/300/300",
-              }}
-            />
-            <Text fontSize={"$lg"} fontWeight={"$bold"}>
-              Objetos
-            </Text>
           </Pressable>
         </View>
         <View flexDirection="column" alignItems="center">
-          <Link
+          <Pressable
             onPress={() => {
+              getImages("Brinquedos");
               setSelectedImage("Brinquedos");
               navigation.navigate("Game");
             }}
@@ -229,23 +191,12 @@ export const FiguresOptions = ({
             <Text fontSize={"$lg"} fontWeight={"$bold"}>
               Brinquedos
             </Text>
-          </Link>
-          <Pressable onPress={()=>{getImages("Brinquedos")}}>
-            <Image
-              size="md"
-              alt="Image1"
-              source={{
-                uri: "http://placekitten.com/300/300",
-              }}
-            />
-            <Text fontSize={"$lg"} fontWeight={"$bold"}>
-              Brinquedos
-            </Text>
           </Pressable>
         </View>
       </View>
       <SafeAreaView></SafeAreaView>
-      <View ref={viewToSnapShotRef}
+      <View
+        ref={viewToSnapShotRef}
         display="flex"
         alignItems="center"
         justifyContent="center"
@@ -253,22 +204,20 @@ export const FiguresOptions = ({
         borderColor="$black"
         mt={4}
         borderWidth={"$1"}
-        w={"80%"}
-        h={"$48"}
-        gap={6}
-        p={3}
+        w={"90%"}
+        h={"55%"}
+        p={2}
       >
-        <Text>Div2</Text>
+        <Text>Draws</Text>
       </View>
       <View
         display="flex"
         alignItems="center"
         justifyContent="space-between"
         flexDirection="row"
-        mt={8}
+        mt={2}
         w={"100%"}
-        gap={6}
-        p={4}
+        h={"12%"}
       >
         <MaterialIcons
           name="arrow-back"
@@ -278,31 +227,32 @@ export const FiguresOptions = ({
             navigation.navigate("Home");
           }}
         />
-        <View
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
+        <View display="flex" alignItems="center" justifyContent="center">
           <Pressable
             display="flex"
             alignItems="center"
             justifyContent="center"
-            onPress={() => {addPoints()}}
+            onPress={() => {
+              addPoints();
+            }}
           >
-            <Image 
+            <Image
               size="md"
               alt="Image1"
               source={require("../../assets/Images/Teste/star.png")}
-              width={256}
-              
+              width={220}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
             />
-            <Text
-              position="absolute"
-            >{points}</Text>
+            <Text position="absolute" fontWeight="bold">
+              {points}
+            </Text>
           </Pressable>
-          
         </View>
-        <Button onPress={snapshot}><MaterialCommunityIcons name="printer" size={32} color="black" /></Button>
+        <Button onPress={snapshot} bg="#f4f4f4">
+          <MaterialCommunityIcons name="printer" size={32} color="black" />
+        </Button>
       </View>
     </View>
   );
--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,11 +1,31 @@
 import { NavigationProp } from "@react-navigation/native";
 
 import { Context } from "../../../App";
-import { View, Text, Button } from "@gluestack-ui/themed";
-import { useContext } from "react";
+import {
+  View,
+  Text,
+  Button,
+  Box,
+  Image,
+  Input,
+  InputField,
+} from "@gluestack-ui/themed";
+import { useContext, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export const Game = ({ navigation }: { navigation: NavigationProp<any> }) => {
   const [selectedImage, setSelectedImage] = useContext(Context);
+  const [word, setWord] = useState<string>("");
+
+  const handleButtonClick = (letter: string) => {
+    setWord((prevValue) =>
+      prevValue == "" ? prevValue + letter.toUpperCase() : prevValue + letter
+    );
+  };
+  const ClearWord = () => {
+    setWord("");
+  };
 
   return (
     <View
@@ -25,8 +45,351 @@ export const Game = ({ navigation }: { navigation: NavigationProp<any> }) => {
         alignItems="center"
         flexDirection="row"
       >
-        <View borderColor="black" borderWidth="$1" w="40%" h="100%"></View>
-        <View borderColor="black" borderWidth="$1" w="60%" h="100%"></View>
+        <View w="40%" h="100%">
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+          >
+            <View
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              w="55%"
+            >
+              <Image
+                borderColor="black"
+                borderWidth="$2"
+                mt="$4"
+                size="xl"
+                alt="Image1"
+                source={{
+                  uri: "http://placekitten.com/300/300",
+                }}
+              />
+            </View>
+            <View display="flex" alignItems="center" justifyContent="center">
+              <Box borderColor="black" borderWidth="$2">
+                <AntDesign name="sound" size={40} color="black" />
+              </Box>
+            </View>
+          </View>
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+            gap="$4"
+            mt="$2"
+          >
+            <Input
+              variant="outline"
+              size="md"
+              w="$40"
+              isReadOnly={true}
+              borderColor="black"
+              borderWidth="$2"
+            >
+              <InputField placeholder="" value={word} fontWeight="$bold" fontSize="$xl"/>
+            </Input>
+            <Box borderColor="black" borderWidth="$2">
+              <AntDesign name="sound" size={40} color="black" />
+            </Box>
+          </View>
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <MaterialIcons
+              name="clear"
+              size={48}
+              color="red"
+              onPress={() => {
+                ClearWord();
+              }}
+            />
+          </View>
+        </View>
+        <View
+          w="55%"
+          h="80%"
+          display="flex"
+          flexDirection="column"
+          borderColor="black"
+          borderWidth="$2"
+          pt="$4"
+          mb="$8"
+        >
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+            gap="$8"
+          >
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("a")}
+              >
+                A
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("b")}
+              >
+                B
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("c")}
+              >
+                C
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("d")}
+              >
+                D
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("e")}
+              >
+                E
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("f")}
+              >
+                F
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("g")}
+              >
+                G
+              </Text>
+            </Box>
+          </View>
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+            gap="$8"
+          >
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("h")}
+              >
+                H
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("i")}
+              >
+                {" "}
+                I
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("j")}
+              >
+                J
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("k")}
+              >
+                K
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("l")}
+              >
+                L
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("m")}
+              >
+                M
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("n")}
+              >
+                N
+              </Text>
+            </Box>
+          </View>
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+            gap="$8"
+          >
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("o")}
+              >
+                O
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("p")}
+              >
+                P
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("q")}
+              >
+                Q
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("r")}
+              >
+                R
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("s")}
+              >
+                S
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("t")}
+              >
+                T
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("u")}
+              >
+                U
+              </Text>
+            </Box>
+          </View>
+          <View
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="row"
+            gap="$8"
+          >
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("v")}
+              >
+                V
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("w")}
+              >
+                W
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("x")}
+              >
+                X
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("y")}
+              >
+                Y
+              </Text>
+            </Box>
+            <Box w="$8">
+              <Text
+                fontSize="$4xl"
+                fontWeight="$bold"
+                onPress={() => handleButtonClick("z")}
+              >
+                Z
+              </Text>
+            </Box>
+          </View>
+        </View>
       </View>
 
       <Button
