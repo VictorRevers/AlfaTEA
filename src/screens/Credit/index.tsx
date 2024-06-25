38856ad2b6c431ed1c2fa05efbe8f5ae8e330dbc
Felihum <felipemedeirosinfo@gmail.com>
2024-06-25 14:33:27 -0300

    Scroll View na tela de Crédito

--- src/screens/Credit/index.tsx
+++ src/screens/Credit/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "@gluestack-ui/themed";
+import { ScrollView, Text, View } from "@gluestack-ui/themed";
 import { ButtonMenu } from "../../components/ButtonPlay";
 import { NavigationProp } from "@react-navigation/native";
 import { Image } from "react-native-svg";
@@ -11,7 +11,9 @@ export function Credit({navigation}: {navigation: NavigationProp<any>}){
                 <Image />
             </View>
             <View style={styles.containerText}>
-                <Text style={styles.txtCredit}>{'AUTORES\n\nRENATA MARQUES COSTA\nE-MAIL: RENATAMARQUES.COSTA@GMAIL.COM\n\nADRIANA DA SILVA LISBOA TOMAZ\nE-MAIL: ATOMAZ@UNICARIOCA.EDU.BR\n\nMARIZA SUELI DE OLIVEIRA SODRÉ\nE-MAIL: MARIZABIO@HOTMAIL.COM\n\nPROGRAMAÇÃO\n\nFELIPE MARTINS DE MEDEIROS\nE-MAIL: FELIPEMEDEIROSINFO@GMAIL.COM\n\nVICTOR REVERS KASNOWSKI\nE-MAIL: VICTOR.KASNOWSKI@GMAIL.COM\n\nMATEUS LOPES DA SILVA\nE-MAIL:MATEUSLOPES1717@GMAIL.COM\n\nLORHAN LUCAS DA SILVA FERNANDES\nE-MAIL:LORHAN123@GMAIL.COM\n\nALICE\nE-MAIL:ALICE-LYRA@HOTMAIL.COM\n\nCOORDENADOR DO LABORATÓRIO DE TECNOLOGIA APLICADA\n\nANDRÉ COTELLI DO ESPÍRITO SANTO\nE-MAIL: ASANTO@UNICARIOCA.EDU.BR'}</Text>
+                <ScrollView>
+                    <Text style={styles.txtCredit}>{'AUTORES\n\nRENATA MARQUES COSTA\nE-MAIL: RENATAMARQUES.COSTA@GMAIL.COM\n\nADRIANA DA SILVA LISBOA TOMAZ\nE-MAIL: ATOMAZ@UNICARIOCA.EDU.BR\n\nMARIZA SUELI DE OLIVEIRA SODRÉ\nE-MAIL: MARIZABIO@HOTMAIL.COM\n\nPROGRAMAÇÃO\n\nFELIPE MARTINS DE MEDEIROS\nE-MAIL: FELIPEMEDEIROSINFO@GMAIL.COM\n\nVICTOR REVERS KASNOWSKI\nE-MAIL: VICTOR.KASNOWSKI@GMAIL.COM\n\nMATEUS LOPES DA SILVA\nE-MAIL:MATEUSLOPES1717@GMAIL.COM\n\nLORHAN LUCAS DA SILVA FERNANDES\nE-MAIL:LORHAN123@GMAIL.COM\n\nALICE LYRA\nE-MAIL:ALICE-LYRA@HOTMAIL.COM\n\nCOORDENADOR DO LABORATÓRIO DE TECNOLOGIA APLICADA\n\nANDRÉ COTELLI DO ESPÍRITO SANTO\nE-MAIL: ASANTO@UNICARIOCA.EDU.BR'}</Text>
+                </ScrollView>
             </View>
             <View style={styles.containerBtnVoltar}>
                 <View>
@@ -27,29 +29,29 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "row",
         margin: 0,
-        padding: 0,
+        padding: 20,
     },
     containerText: {
-        width: 374,
+        width: "50%",
         height: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
     },
     containerImagem: {
-        width: 200,
+        width: "25%",
         height: "100%",
     },
     containerBtnVoltar: {
         display: "flex",
         justifyContent: "flex-end",
         alignItems: "flex-end",
-        width: 200,
-        height: "99%",
+        width: "25%",
+        height: "100%",
     },
     txtCredit: {
         textAlign: "left",
-        fontSize: 9,
+        fontSize: 14,
         fontWeight: "bold",
     },
 });
